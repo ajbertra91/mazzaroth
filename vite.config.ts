4ef97d7cc2b0e394f895d098349669897208630c
Adam Bertrand <ajbertra91@gmail.com>
2024-12-28 12:31:45 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,26 +5,24 @@ import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    base: mode === 'development' ? '' : '/mazzaroth/',
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@components': path.resolve(__dirname, './src/components'),
-        '@hooks': path.resolve(__dirname, './src/hooks'),
-        '@pages': path.resolve(__dirname, './src/pages'),
-        '@appTypes': path.resolve(__dirname, './src/types'),
-        '@utils': path.resolve(__dirname, './src/utils'),
-      },
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@hooks': path.resolve(__dirname, './src/hooks'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@appTypes': path.resolve(__dirname, './src/types'),
+      '@utils': path.resolve(__dirname, './src/utils'),
     },
-    css: {
-      postcss: {
-        plugins: [tailwindcss(), autoprefixer()],
-      },
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
     },
-    server: {
-      port: 3000,
-    },
-  };
+  },
+  server: {
+    port: 3000,
+  },
 });
