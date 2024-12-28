a6b99e60b6cacc4c098b83673e5d98e456e0b270
Adam Bertrand <ajbertra91@gmail.com>
2024-12-28 12:37:31 -0500

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,25 +4,27 @@ import path from 'path';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@appTypes': path.resolve(__dirname, './src/types'),
-      '@utils': path.resolve(__dirname, './src/utils'),
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/mazzaroth/' : '/', // Use '/' for development and '/mazzaroth/' for production
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@components': path.resolve(__dirname, './src/components'),
+        '@hooks': path.resolve(__dirname, './src/hooks'),
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@appTypes': path.resolve(__dirname, './src/types'),
+        '@utils': path.resolve(__dirname, './src/utils'),
+      },
     },
-  },
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
+    css: {
+      postcss: {
+        plugins: [tailwindcss(), autoprefixer()],
+      },
     },
-  },
-  server: {
-    port: 3000,
-  },
+    server: {
+      port: 3000, // Run on localhost:3000
+    },
+  };
 });
